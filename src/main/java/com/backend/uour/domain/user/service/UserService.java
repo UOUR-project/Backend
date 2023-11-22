ce9fe122ebaad0c281e0a311f0766369897d089f
minseokey <lms990427@kookmin.ac.kr>
2023-11-22 21:53:52 +0900

    feat: 로그인 부가기능 추가

--- src/main/java/com/backend/uour/domain/user/service/UserService.java
+++ src/main/java/com/backend/uour/domain/user/service/UserService.java
@@ -1,9 +1,11 @@
 package com.backend.uour.domain.user.service;
 
 import com.backend.uour.domain.user.dto.AdditionUserSignUpDto;
+import com.backend.uour.domain.user.dto.MypageDto;
 import com.backend.uour.domain.user.dto.UserSignUpDto;
 import com.backend.uour.domain.user.dto.UserUpdateDto;
 import com.backend.uour.domain.user.entity.BLAME_CATEGORY;
+import com.backend.uour.domain.user.entity.ROLE;
 
 public interface UserService {
     void signUp(UserSignUpDto userSignUpDto) throws Exception;
@@ -12,4 +14,9 @@ public interface UserService {
     void updateUser(UserUpdateDto updateDto, String temp) throws Exception;
     boolean blame(Long pointed_outId, String authorization, BLAME_CATEGORY blameCategory) throws Exception;
     void promoteAdmin(Long pointedId, String authorization) throws Exception;
+
+    MypageDto myPage(String authorization) throws Exception;
+    ROLE getRole(String authorization) throws Exception;
+    boolean emailCheck(String email) throws Exception;
+    void initialSelfPromoteAdmin(String authorization) throws Exception;
 }
