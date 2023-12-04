9b1796ac88d2553c998c633b21cf9a498b1c1ba6
minseokey <lms990427@kookmin.ac.kr>
2023-12-04 11:18:30 +0900

    fix: 유저DTO 에 대학교가 보이도록 추가

--- src/main/java/com/backend/uour/domain/community/dto/AuthorDto.java
+++ src/main/java/com/backend/uour/domain/community/dto/AuthorDto.java
@@ -1,5 +1,6 @@
 package com.backend.uour.domain.community.dto;
 
+import com.backend.uour.domain.user.entity.SCHOOL;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +11,6 @@ import lombok.RequiredArgsConstructor;
 @Builder
 public class AuthorDto {
     private String nickname;
+    private SCHOOL school;
     private Long id;
 }
